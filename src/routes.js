5f321bd0909c2eddce376b0e534bf3f652a2f582
Huang Chi-Han <huangchiheng@gmail.com>
2016-02-20 11:12:48 +0800

    remove sample route, this route is just for experimenting

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { IndexRoute, Route, hashHistory } from 'react-router';
 // import App from './containers/App/App';
 // import Home from './containers/Home/Home';
-import { App, Home, Sample } from 'containers';
+import { App, Home } from 'containers';
 
-
-
-// console.log(Home);
 /*
  * 1. missing a header
  * 2. missing a footer
@@ -18,7 +15,7 @@ export default () => {
 
       { /* Routes */ }
       <Route path="home" component={Home} />
-      <Route path="sample" component={Sample} />
+
     </Route>
   );
 };
